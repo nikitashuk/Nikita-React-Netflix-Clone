0366d473af1be275c1a042dba407f65370e2b696
Nikita Shukla <nikita.shukla@paavu.com>
2024-05-30 13:19:28 +0530

    vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./components/pages/Home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/pages/Login/Login";
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route path="*" element={<Navigate to={"/"} replace />} />
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
